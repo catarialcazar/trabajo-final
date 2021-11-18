8848ccc477b2a2c8dd289224e7c2baed3e0c0eb5
Catari02 <89741808+Catari02@users.noreply.github.com>
2021-11-18 17:32:34 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -48,17 +48,3 @@ function backToTop() {
     offcanvasCollapse.classList.toggle("open");
   });
 })();
-
-// Submit AJAX:
-$.ajax({
-  method: 'POST',
-  url: 'https://formsubmit.co/ajax/your@email.com',
-  dataType: 'json',
-  accepts: 'application/json',
-  data: {
-      name: "FormSubmit",
-      message: "I'm from Devro LABS"
-  },
-  success: (data) => console.log(data),
-  error: (err) => console.log(err)
-});

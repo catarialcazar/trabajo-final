cca2fa478698995e37c82071792c6e11b49a5021
Catari02 <89741808+Catari02@users.noreply.github.com>
2021-11-23 10:55:13 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -16,44 +16,33 @@ for (const str of content) {
 
 instance.go();
 
-
-
-
 // Back to Top:
 let mybutton = document.getElementById("btn-back-to-top");
 
 window.onscroll = function () {
-  scrollFunction();
+    scrollFunction();
 };
 
 function scrollFunction() {
-  if (
-    document.body.scrollTop > 80 ||
-    document.documentElement.scrollTop > 80
-  ) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
 }
 mybutton.addEventListener("click", backToTop);
 
 function backToTop() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-};
-
-
-
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
 
 // Offcanvas menu:
 (function mainScript() {
-  "use strict";
-  const offcanvasToggle = document.querySelector(
-    '[data-bs-toggle="offcanvas"]',
-  );
-  const offcanvasCollapse = document.querySelector(".offcanvas-collapse");
-  offcanvasToggle.addEventListener("click", function () {
-    offcanvasCollapse.classList.toggle("open");
-  });
+    "use strict";
+    const offcanvasToggle = document.querySelector('[data-bs-toggle="offcanvas"]');
+    const offcanvasCollapse = document.querySelector(".offcanvas-collapse");
+    offcanvasToggle.addEventListener("click", function () {
+        offcanvasCollapse.classList.toggle("open");
+    });
 })();

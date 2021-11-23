1ad3e639da51294d6e44b3daa0d21928514fa22f
Catari02 <89741808+Catari02@users.noreply.github.com>
2021-11-22 22:22:30 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -23,24 +23,21 @@ instance.go();
 let mybutton = document.getElementById("btn-back-to-top");
 
 window.onscroll = function () {
-  scrollFunction();
+    scrollFunction();
 };
 
 function scrollFunction() {
-  if (
-    document.body.scrollTop > 80 ||
-    document.documentElement.scrollTop > 80
-  ) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
 }
 mybutton.addEventListener("click", backToTop);
 
 function backToTop() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
 
 
@@ -55,25 +52,5 @@ function backToTop() {
   const offcanvasCollapse = document.querySelector(".offcanvas-collapse");
   offcanvasToggle.addEventListener("click", function () {
     offcanvasCollapse.classList.toggle("open");
-    alert("alerta");
-    offcanvasCollapse.classList.toggle("close");
   });
 })();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-

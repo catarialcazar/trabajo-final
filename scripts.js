804e88d9b2ca9536349ff8709c68e92e6d860844
Catari02 <89741808+Catari02@users.noreply.github.com>
2021-11-23 10:34:03 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -23,22 +23,25 @@ instance.go();
 let mybutton = document.getElementById("btn-back-to-top");
 
 window.onscroll = function () {
-    scrollFunction();
+  scrollFunction();
 };
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
-    }
+  if (
+    document.body.scrollTop > 80 ||
+    document.documentElement.scrollTop > 80
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
 }
 mybutton.addEventListener("click", backToTop);
 
 function backToTop() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+};
 
 
 
